7e200db33ae88b16514d455df674db567aa488a9
martinShahinyan <martin.shahinyan2008@gmail.com>
2025-11-25 22:35:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,12 @@ async def create_assignment(
     title1: Annotated[str, Query(title="name of title")],
     status1: Annotated[str, Query(title="status of title (Done,Undone)")],
     estimated_time1: Annotated[int, Query(title="time to complete assignment in hours", ge=1, lt=50)]
-)
+):
     db = SessionLocal()
     new_assignment = Assignments(title=title1, status=status1, estimated_time=estimated_time1)
     db.add(new_assignment)
     db.commit()
+    db.refresh(new_assignment)
     db.close()
     return {
         "Message": "new assignment created",
@@ -87,7 +88,7 @@ async def update_assignment(
     new_title: Annotated[str, Query(title="new title of assignment")],
     new_status: Annotated[str, Query(title="new status of assignment (Done,Undone)")],
     new_estimated_time: Annotated[int, Query(title="new estimated time", ge=1)]
-)
+):
     db = SessionLocal()
     res = db.query(Assignments).filter(Assignments.id == assign_id).first()
     if not res:
@@ -119,12 +120,14 @@ async def update_some_assignment(
     if not res:
         db.close()
         raise HTTPException(status_code=404, detail="no assignment with this id")
+
     if new_title is not None:
         res.title = new_title
     if new_status is not None:
         res.status = new_status
     if new_estimated_time is not None:
         res.estimated_time = new_estimated_time
+
     db.commit()
     db.refresh(res)
     db.close()
